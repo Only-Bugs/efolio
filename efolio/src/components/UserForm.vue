90d4538847acb4f53dbda5d9e785376f02a6c7a1
Shashank Prabhakar Reddy <pshashank0502@gmail.com>
2025-08-22 20:08:51 +1000

    Install, import and setup datatable with PrimeVue
    Signed-off-by: Shashank Prabhakar Reddy <pshashank0502@gmail.com>

--- efolio/src/components/UserForm.vue
+++ efolio/src/components/UserForm.vue
@@ -89,25 +89,20 @@
         </form>
 
         <div class="row mt-5" v-if="submittedCards.length">
-          <div class="d-flex flex-wrap justify-content-start">
-            <div
-              v-for="(card, index) in submittedCards"
-              :key="index"
-              class="card m-2"
-              style="width: 18rem"
-            >
-              <div class="card-header">User Information</div>
-              <ul class="list-group list-group-flush">
-                <li class="list-group-item">Username: {{ card.username }}</li>
-                <li class="list-group-item">Password: {{ card.password }}</li>
-                <li class="list-group-item">
-                  Australian Resident: {{ card.isAustralian ? "Yes" : "No" }}
-                </li>
-                <li class="list-group-item">Gender: {{ card.gender }}</li>
-                <li class="list-group-item">Reason: {{ card.reason }}</li>
-              </ul>
-            </div>
-          </div>
+          <DataTable
+            :value="submittedCards"
+            class="p-datatable-sm"
+            responsiveLayout="scroll"
+          >
+            <Column field="username" header="Username"></Column>
+            <Column field="password" header="Password"></Column>
+            <Column
+              header="Resident"
+              :body="(row) => (row.isAustralian ? 'Yes' : 'No')"
+            ></Column>
+            <Column field="gender" header="Gender"></Column>
+            <Column field="reason" header="Reason"></Column>
+          </DataTable>
         </div>
       </div>
     </div>
@@ -217,23 +212,3 @@ const clearForm = () => {
   };
 };
 </script>
-
-<style scoped>
-.card {
-  border: 1px solid #ccc;
-  border-radius: 10px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-}
-.card-header {
-  background-color: #275fda;
-  color: white;
-  padding: 10px;
-  border-radius: 10px 10px 0 0;
-}
-.list-group-item {
-  padding: 10px;
-}
-.text-danger {
-  font-size: 0.9rem;
-}
-</style>
--- efolio/src/main.js
+++ efolio/src/main.js
@@ -1,6 +1,23 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-// import "./style.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-createApp(App).mount("#app");
+import PrimeVue from "primevue/config";
+import Aura from "@primeuix/themes/aura";
+import DataTable from "primevue/datatable";
+import Column from "primevue/column";
+
+const app = createApp(App);
+
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      darkModeSelector: false,
+    },
+  },
+});
+app.component("DataTable", DataTable);
+app.component("Column", Column);
+
+app.mount("#app");
